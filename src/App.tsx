97786b130bd97b55759f6ab269a7c6c40a02f280
Kevin N <kevin.nove@gmail.com>
2025-07-22 15:46:55 -0400

    Simplify codebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import CustomButton from "./CustomButton";
 
 function App() {
@@ -9,30 +6,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <CustomButton
-          onClick={() => {
-            setCount((count) => count + 1);
-          }}
-        >
-          count is {count}
-        </CustomButton>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        Vite + Vitest (and testing-library) + React + Mui7 + Styled-components
+      </h1>
+      <CustomButton
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        count is {count}
+      </CustomButton>
     </>
   );
 }

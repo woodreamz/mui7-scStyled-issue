2f0c31a2133c387f50f134c6662d46b2e66a9fcf
Kevin N <kevin.nove@gmail.com>
2025-07-22 14:15:11 -0400

    Add mui + styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CustomButton from "./Button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,9 +19,13 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <CustomButton
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
-        </button>
+        </CustomButton>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,8 @@
+import { Button, type ButtonProps } from "@mui/material";
+
+const CustomButton = (props: ButtonProps) => {
+  return <Button {...props} />;
+};
+// #endregion
+
+export default CustomButton;
